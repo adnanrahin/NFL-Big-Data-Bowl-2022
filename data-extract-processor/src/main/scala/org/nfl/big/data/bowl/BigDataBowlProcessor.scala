867f23d581b85161b48c6a352a1dec2492d77cc7
adnanrahin <adnan.rahin13@gmail.com>
2021-10-08 15:52:58 -0400

    Data Tested

--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/BigDataBowlProcessor.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/BigDataBowlProcessor.scala
@@ -4,6 +4,7 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.nfl.big.data.bowl.constant.Constant
+import org.nfl.big.data.bowl.dataextractors.TrackingDataExtractor
 import org.nfl.big.data.bowl.dataloader._
 import org.nfl.big.data.bowl.entity._
 
@@ -24,40 +25,18 @@ object BigDataBowlProcessor {
     val dataPath = args(0)
 
     val gameDataLoader: GameDataLoader = new GameDataLoader(dataPath + Constant.GAMES, spark)
-/*
     val pffScoutingDataLoader: PFFScoutingDataLoader = new PFFScoutingDataLoader(dataPath + Constant.PFFSCOUNTINGDATA, spark)
     val playersDataLoader: PlayersDataLoader = new PlayersDataLoader(dataPath + Constant.PLAYERS, spark)
     val playsDataLoader: PlaysDataLoader = new PlaysDataLoader(dataPath + Constant.PLAYS, spark)
     val trackingDataLoader: TrackingDataLoader = new TrackingDataLoader(dataPath + Constant.TRACKING, spark)
-*/
 
     val gameRDD: RDD[Games] = gameDataLoader.loadRDD()
-/*    val pffScoutingRDD: RDD[PFFScoutingData] = pffScoutingDataLoader.loadRDD()
+    val pffScoutingRDD: RDD[PFFScoutingData] = pffScoutingDataLoader.loadRDD()
     val playersRDD: RDD[Players] = playersDataLoader.loadRDD()
     val playsRDD: RDD[Plays] = playsDataLoader.loadRDD()
-    val trackingRDD: RDD[Tracking] = trackingDataLoader.loadRDD()*/
+    val trackingRDD: RDD[Tracking] = trackingDataLoader.loadRDD()
 
-    gameRDD.foreach(trackt => println(trackt))
-
-    //findEventByEventNameToDF("none", trackingRDD, spark)
-
-  }
-
-  private def findEventByEventName(event: String, trackingRDD: RDD[Tracking]): RDD[Tracking] = {
-
-    val events: RDD[Tracking] =
-      trackingRDD.filter(track => track.event.toString.equals(event))
-
-    events/*.persist(StorageLevel.MEMORY_AND_DISK)*/
-  }
-
-  def findEventByEventNameToDF(event: String, trackingRDD: RDD[Tracking], spark: SparkSession): Unit = {
-
-    val events: RDD[Tracking] = findEventByEventName(event, trackingRDD)
-
-    spark
-      .createDataFrame(events)
-      .show(5, truncate = false)
+    TrackingDataExtractor.findEventByEventNameToDF("none", trackingRDD, spark)
 
   }
 
--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataextractors/TrackingDataExtractor.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataextractors/TrackingDataExtractor.scala
@@ -2,6 +2,7 @@ package org.nfl.big.data.bowl.dataextractors
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
 import org.nfl.big.data.bowl.entity.Tracking
 
 object TrackingDataExtractor {
@@ -9,9 +10,9 @@ object TrackingDataExtractor {
   private def findEventByEventName(event: String, trackingRDD: RDD[Tracking]): RDD[Tracking] = {
 
     val events: RDD[Tracking] =
-      trackingRDD.filter(track => track.event.toString.equals(event))
+      trackingRDD.filter(track => track.event.equalsIgnoreCase(event))
 
-    events/*.persist(StorageLevel.MEMORY_AND_DISK)*/
+    events.persist(StorageLevel.MEMORY_AND_DISK)
   }
 
   def findEventByEventNameToDF(event: String, trackingRDD: RDD[Tracking], spark: SparkSession): Unit = {
@@ -21,7 +22,6 @@ object TrackingDataExtractor {
     spark
       .createDataFrame(events)
       .show(truncate = false)
-
   }
 
 }
