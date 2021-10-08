16aea54e203ea31a31cb8ccd4cba4c9851573d71
adnanrahin <adnan.rahin13@gmail.com>
2021-10-08 15:43:59 -0400

    Plays Data String filter Added

--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataloader/PlaysDataLoader.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataloader/PlaysDataLoader.scala
@@ -14,14 +14,38 @@ class PlaysDataLoader(filePath: String, spark: SparkSession) extends DataLoader 
 
     val playsDataRDD: RDD[Plays] = playsDataCSV
       .map(row => row.split(",", -1))
-      .map(str => Plays(str(0),
-        str(1), str(2), str(3), str(4), str(5), str(6),
-        str(7), str(8), str(9), str(10), str(1), str(12),
-        str(13), str(14), str(15), str(16), str(17),
-        str(18), str(19), str(20), str(21), str(22), str(23),
-        str(24))).mapPartitionsWithIndex {
-      (idx, iter) => if (idx == 0) iter.drop(1) else iter
-    }
+      .map(str =>
+        Plays(
+          str(0).replace("\"", ""),
+          str(1).replace("\"", ""),
+          str(2).replace("\"", ""),
+          str(3).replace("\"", ""),
+          str(4).replace("\"", ""),
+          str(5).replace("\"", ""),
+          str(6).replace("\"", ""),
+          str(7).replace("\"", ""),
+          str(8).replace("\"", ""),
+          str(9).replace("\"", ""),
+          str(10).replace("\"", ""),
+          str(11).replace("\"", ""),
+          str(12).replace("\"", ""),
+          str(13).replace("\"", ""),
+          str(14).replace("\"", ""),
+          str(15).replace("\"", ""),
+          str(16).replace("\"", ""),
+          str(17).replace("\"", ""),
+          str(18).replace("\"", ""),
+          str(19).replace("\"", ""),
+          str(20).replace("\"", ""),
+          str(21).replace("\"", ""),
+          str(22).replace("\"", ""),
+          str(23).replace("\"", ""),
+          str(24).replace("\"", "")
+        )
+      )
+      .mapPartitionsWithIndex {
+        (idx, iter) => if (idx == 0) iter.drop(1) else iter
+      }
 
     playsDataRDD.persist(StorageLevel.MEMORY_AND_DISK)
 
