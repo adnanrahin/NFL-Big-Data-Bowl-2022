7e90b232a68725fdfd5d9fa2504b047d27c147f8
adnanrahin <adnan.rahin13@gmail.com>
2021-10-08 15:45:36 -0400

    Tracking Filterd From quotes

--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataloader/TrackingDataLoader.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataloader/TrackingDataLoader.scala
@@ -4,7 +4,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 import org.nfl.big.data.bowl.DataLoader
-import org.nfl.big.data.bowl.entity.{Plays, Tracking}
+import org.nfl.big.data.bowl.entity.Tracking
 
 class TrackingDataLoader(filePath: String, spark: SparkSession) extends DataLoader {
 
@@ -14,12 +14,31 @@ class TrackingDataLoader(filePath: String, spark: SparkSession) extends DataLoad
 
     val trackingDataRDD: RDD[Tracking] = trackingDataCSV
       .map(row => row.split(",", -1))
-      .map(str => Tracking(str(0),
-        str(1), str(2), str(3), str(4), str(5), str(6),
-        str(7), str(8), str(9), str(10), str(1), str(12),
-        str(13), str(14), str(15), str(16), str(17))).mapPartitionsWithIndex {
-      (idx, iter) => if (idx == 0) iter.drop(1) else iter
-    }
+      .map(str =>
+        Tracking(
+          str(0).replace("\"", ""),
+          str(1).replace("\"", ""),
+          str(2).replace("\"", ""),
+          str(3).replace("\"", ""),
+          str(4).replace("\"", ""),
+          str(5).replace("\"", ""),
+          str(6).replace("\"", ""),
+          str(7).replace("\"", ""),
+          str(8).replace("\"", ""),
+          str(9).replace("\"", ""),
+          str(10).replace("\"", ""),
+          str(11).replace("\"", ""),
+          str(12).replace("\"", ""),
+          str(13).replace("\"", ""),
+          str(14).replace("\"", ""),
+          str(15).replace("\"", ""),
+          str(16).replace("\"", ""),
+          str(17).replace("\"", "")
+        )
+      )
+      .mapPartitionsWithIndex {
+        (idx, iter) => if (idx == 0) iter.drop(1) else iter
+      }
 
     trackingDataRDD.persist(StorageLevel.MEMORY_AND_DISK)
 
