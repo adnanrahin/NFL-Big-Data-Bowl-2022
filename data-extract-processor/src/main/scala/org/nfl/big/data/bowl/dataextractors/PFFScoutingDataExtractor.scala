61d1894e2e320afbc3cbfae535b06e3b90562bef
adnanrahin <adnan.rahin13@gmail.com>
2021-10-26 23:20:57 -0400

    update

--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataextractors/PFFScoutingDataExtractor.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/dataextractors/PFFScoutingDataExtractor.scala
@@ -1,7 +1,35 @@
 package org.nfl.big.data.bowl.dataextractors
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import org.nfl.big.data.bowl.DataProcessorHelper.isNumeric
+import org.nfl.big.data.bowl.entity.PFFScoutingData
+
 object PFFScoutingDataExtractor {
 
+  final val NA = "NA"
+
+  def findTotalHangTimeInEachGame(pffScoutingRDD: RDD[PFFScoutingData]): RDD[(String, Double)] = {
+
+    val result: RDD[(String, Double)] =
+
+      pffScoutingRDD.filter(pf => !pf.hangTime.equalsIgnoreCase(NA))
+        .filter(t => isNumeric(t.hangTime))
+        .map(pf => (pf.gameId, pf.hangTime.toDouble))
+        .groupBy(_._1)
+        .map {
+          t =>
+            (
+              t._1, BigDecimal
+              .apply(t._2.foldLeft(0.0)(_ + _._2))
+              .setScale(6, BigDecimal.RoundingMode.HALF_EVEN)
+              .toDouble
+            )
+        }
+        .sortBy(_._2)
+
+    result.persist(StorageLevel.MEMORY_AND_DISK)
+  }
 
 
 }
