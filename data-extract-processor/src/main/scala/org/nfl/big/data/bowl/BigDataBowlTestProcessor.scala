f89c2a5356547f683560e6115bc223f5fd8fcb3e
adnanrahin <adnan.rahin13@gmail.com>
2021-11-04 21:54:07 -0400

    Test Porcessor Fixed

--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/BigDataBowlTestProcessor.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/BigDataBowlTestProcessor.scala
@@ -4,7 +4,6 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.nfl.big.data.bowl.constant.Constant
-import org.nfl.big.data.bowl.dataextractors.PFFScoutingDataExtractor
 import org.nfl.big.data.bowl.dataloader.PFFScoutingDataLoader
 import org.nfl.big.data.bowl.entity.PFFScoutingData
 
@@ -28,7 +27,7 @@ object BigDataBowlTestProcessor {
 
     val pffScoutingRDD: RDD[PFFScoutingData] = pffScoutingDataLoader.loadRDD()
 
-    PFFScoutingDataExtractor.extractPuntRushers(pffScoutingRDD)
+    /*PFFScoutingDataExtractor.extractPuntRushers(pffScoutingRDD)*/
     
   }
 
