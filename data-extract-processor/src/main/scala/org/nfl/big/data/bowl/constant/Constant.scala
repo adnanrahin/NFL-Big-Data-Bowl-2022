8300d9064e026513cb0775e06427c15847f181b6
adnanrahin <adnan.rahin13@gmail.com>
2024-02-25 17:39:05 -0500

    update

--- data-extract-processor/src/main/scala/org/nfl/big/data/bowl/constant/Constant.scala
+++ data-extract-processor/src/main/scala/org/nfl/big/data/bowl/constant/Constant.scala
@@ -6,7 +6,7 @@ object Constant {
   final val PFFSCOUNTINGDATA = "PFFScoutingData.csv"
   final val PLAYERS = "players.csv"
   final val PLAYS = "plays.csv"
-  final val TRACKING = "tracking\\*"
+  final val TRACKING = "tracking/*.csv"
   final val FILTER_DIR = "filterdata"
   final val PIPE_DELIMITER = "|"
   final val SEMICOLON_DELIMITER = ";"
